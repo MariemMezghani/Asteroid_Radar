7df809bb35e0e1031676c0be9232cc7e1e6e20aa
MariemMezghani <35550711+MariemMezghani@users.noreply.github.com>
2021-01-22 08:22:19 +0100

    parse first asteroid in the list

--- app/src/main/java/com/github/mariemmezghani/asteroidradar/api/AsteroidsApiInterface.kt
+++ app/src/main/java/com/github/mariemmezghani/asteroidradar/api/AsteroidsApiInterface.kt
@@ -1,21 +1,31 @@
 package com.github.mariemmezghani.asteroidradar.api
 
+import com.github.mariemmezghani.asteroidradar.Asteroid
 import com.github.mariemmezghani.asteroidradar.Constants
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.ResponseBody
+import org.json.JSONObject
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import retrofit2.http.GET
 
-private val retrofit = Retrofit.Builder().addConverterFactory(ScalarsConverterFactory.create())
+private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(Constants.BASE_URL).build()
 
+
 interface AsteroidsApiInterface {
-    @GET("neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=${Constants.API_KEY}")
-    fun getProperties(): Call<String>
+    @GET("neo/rest/v1/feed?api_key=${Constants.API_KEY}")
+    fun getProperties(): String
 }
 
 object AsteroidsApi {
-    val retrofitservice: AsteroidsApiInterface by lazy {
+    val retrofitService: AsteroidsApiInterface by lazy {
         retrofit.create(AsteroidsApiInterface::class.java)
     }
 }
--- app/src/main/java/com/github/mariemmezghani/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/github/mariemmezghani/asteroidradar/main/MainViewModel.kt
@@ -3,10 +3,17 @@ package com.github.mariemmezghani.asteroidradar.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.mariemmezghani.asteroidradar.Asteroid
 import com.github.mariemmezghani.asteroidradar.api.AsteroidsApi
+import com.github.mariemmezghani.asteroidradar.api.parseAsteroidsJsonResult
+import okhttp3.ResponseBody
+import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
 
 class MainViewModel : ViewModel() {
     // The internal MutableLiveData String that stores the status of the most recent request
@@ -24,16 +31,16 @@ class MainViewModel : ViewModel() {
     }
 
     private fun getData() {
-        AsteroidsApi.retrofitservice.getProperties().enqueue(object : Callback<String> {
+        viewModelScope.launch {
+            try {
+                var result = AsteroidsApi.retrofitService.getProperties()
+                val json = JSONObject(result)
+                _response.value = parseAsteroidsJsonResult(json).get(0).codename
+            } catch (e: Exception) {
+                _response.value = "Failure: " + e.message
 
-            override fun onFailure(call: Call<String>, t: Throwable) {
-                _response.value = "Failure: " + t.message
             }
 
-            override fun onResponse(call: Call<String>, response: Response<String>) {
-                _response.value = response.body()
-            }
-
-        })
+        }
     }
 }
